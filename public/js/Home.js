55c22b6027c6ce4c125599c2ee8e98ff9cc2e6cf
baten005 <134430653+baten005@users.noreply.github.com>
2023-10-06 23:06:48 +0600

    Your commit message here

--- public/js/Home.js
+++ public/js/Home.js
@@ -3,7 +3,7 @@ const div1 = document.querySelectorAll('#main_nav_container div');
 for (let index = 0; index < div1.length; index++) {
     
     div1[index].addEventListener('mouseover', function () {
-            ul1[index].style.maxHeight = '10rem'; 
+            ul1[index].style.maxHeight = '15rem'; 
         });
 
         div1[index].addEventListener('mouseout', function () {
@@ -18,18 +18,16 @@ window.addEventListener("scroll", function() {
     var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
     if (currentScroll > lastScrollTop) {
-        // Scroll down
         document.getElementById("secondary_nav").classList.add("secondary_nav_hidden");
         document.getElementById("Logo_Name").classList.add("collapsed");
         document.getElementById("main_nav").classList.add("collapsed");
     } else {
-        // Scroll up
         document.getElementById("secondary_nav").classList.remove("secondary_nav_hidden");
         document.getElementById("Logo_Name").classList.remove("collapsed");
         document.getElementById("main_nav").classList.remove("collapsed");
         
     }
 
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For mobile browsers
+    
 });
 
