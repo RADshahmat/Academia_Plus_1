3edefec1707efdbc6b0b361e0c57d0bac08c05dd
baten005 <134430653+baten005@users.noreply.github.com>
2023-12-12 04:24:57 +0600

    Admission test okk, Added Session

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const oracledb = require("oracledb");
 const session = require("express-session");
 const oracleDbStore = require("express-oracle-session2")(session);
 const MySQLStore = require("express-mysql-session")(session);
+const server = http.createServer(app);
+const io = socketIo(server);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.static("uploadimage"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(express.json());
+var screenWidth;
+
+app.post("/setScreenWidth", (req, res) => {
+  screenWidth = req.body.screenWidth;
+  console.log("Received screen width:", screenWidth);
+
+  res.sendStatus(200);
+});
 //////////// start of session ////////////////////////////////
 
 const options = {
@@ -49,24 +66,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 ////////////////////////////////////File Upload End///////////////////
-
-app.use(express.urlencoded({ extended: true }));
-const server = http.createServer(app);
-const io = socketIo(server);
-app.use(express.static("public"));
-app.use(express.static("uploadimage"));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(bodyParser.json());
-app.use(express.json());
-var screenWidth;
-
-app.post("/setScreenWidth", (req, res) => {
-  screenWidth = req.body.screenWidth;
-  console.log("Received screen width:", screenWidth);
-
-  res.sendStatus(200);
-});
 
 //////////////////////Database Connection Strat/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let connectionPool;
@@ -126,142 +125,389 @@ app.get("/", async function (req, res) {
 });
 
 app.get("/index", async function (req, res) {
-  const result = await run('SELECT * FROM "ACADEMIA_PLUS_NEW"."APPLICANTS"');
-  console.log(result.data);
-  if (result.success) {
-    console.log(result.data);
-    res.render("index", { result: result.data, db_stat: result.success });
-  } else {
-    res.render("index", { result: result.data, db_stat: result.success });
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("index", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("index", { logged_in: false });
+    }
+  } catch {
+    res.render("index", { logged_in: false });
   }
 });
 
 app.get("/admission", function (req, res) {
-  res.render("admission");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("admission", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("admission", { logged_in: false });
+    }
+  } catch {
+    res.render("admission", { logged_in: false });
+  }
 });
+
 app.get("/events", function (req, res) {
-  res.render("events");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("events", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("events", { logged_in: false });
+    }
+  } catch {
+    res.render("events", { logged_in: false });
+  }
 });
+
 app.get("/history", function (req, res) {
-  res.render("history");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("history", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("history", { logged_in: false });
+    }
+  } catch {
+    res.render("history", { logged_in: false });
+  }
 });
+
 app.get("/achievements", function (req, res) {
-  res.render("achievements");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("achievements", {
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } else {
+      res.render("achievements", { logged_in: false });
+    }
+  } catch {
+    res.render("achievements", { logged_in: false });
+  }
 });
 
 app.get("/facultyStaffInfo", function (req, res) {
-  res.render("facultyStaffInfo");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("facultyStaffInfo", {
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } else {
+      res.render("facultyStaffInfo", { logged_in: false });
+    }
+  } catch {
+    res.render("facultyStaffInfo", { logged_in: false });
+  }
 });
+
 app.get("/message", function (req, res) {
-  res.render("message");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("message", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("message", { logged_in: false });
+    }
+  } catch {
+    res.render("message", { logged_in: false });
+  }
 });
+
 app.get("/governingbody", function (req, res) {
-  res.render("governingbody");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("governingbody", {
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } else {
+      res.render("governingbody", { logged_in: false });
+    }
+  } catch {
+    res.render("governingbody", { logged_in: false });
+  }
 });
+
 app.get("/admin", function (req, res) {
-  res.render("admin");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("admin", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.redirect("log_in");
+    }
+  } catch {
+    res.redirect("log_in");
+  }
 });
+
 app.get("/GuidelineforParents", async function (req, res) {
   const gline = await run(
     'SELECT * FROM "ACADEMIA_PLUS_NEW"."GUIDELINE_FOR_PARRENTS"'
   );
   console.log(gline.data);
+
   if (gline.success) {
-    console.log(gline.data);
-    res.render("GuidelineforParents", {
-      gline: gline.data,
-      db_stat: gline.success,
-    });
+    try {
+      res.render("GuidelineforParents", {
+        gline: gline.data,
+        db_stat: gline.success,
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } catch {
+      res.render("GuidelineforParents", {
+        gline: gline.data,
+        db_stat: gline.success,
+        logged_in: false,
+      });
+    }
   } else {
-    res.render("GuidelineforParents", {
-      gline: gline.data,
-      db_stat: gline.success,
-    });
+    try {
+      res.render("GuidelineforParents", {
+        gline: gline.data,
+        db_stat: gline.success,
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } catch {
+      res.render("GuidelineforParents", {
+        gline: gline.data,
+        db_stat: gline.success,
+        logged_in: false,
+      });
+    }
   }
 });
+
 app.get("/library", function (req, res) {
-  res.render("library");
-});
-app.get("/clubs", function (req, res) {
-  res.render("clubs");
-});
-app.get("/notice", function (req, res) {
-  res.render("notice");
-});
-app.get("/contact", function (req, res) {
-  res.render("contact");
-});
-app.get("/codeofconduct", function (req, res) {
-  res.render("codeofconduct");
-});
-app.get("/routine", function (req, res) {
-  res.render("routine");
-});
-app.get("/photos", function (req, res) {
-  res.render("photos");
-});
-app.get("/videos", function (req, res) {
-  res.render("videos");
-});
-app.get("/calender", function (req, res) {
-  res.render("calender");
-});
-app.get("/cocur", function (req, res) {
-  res.render("cocur");
-});
-app.get("/dress", function (req, res) {
-  res.render("dress");
-});
-app.get("/apply", function (req, res) {
-  res.render("admission/apply");
-});
-app.get("/applicant_dashboard", function (req, res) {
-  if (!req.session.user.isAuthenticated) {
-    res.redirect("log_in.ejs");
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("library", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("library", { logged_in: false });
+    }
+  } catch {
+    res.render("library", { logged_in: false });
   }
+});
+
+app.get("/clubs", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("clubs", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("clubs", { logged_in: false });
+    }
+  } catch {
+    res.render("clubs", { logged_in: false });
+  }
+});
+
+app.get("/notice", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("notice", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("notice", { logged_in: false });
+    }
+  } catch {
+    res.render("notice", { logged_in: false });
+  }
+});
+
+app.get("/contact", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("contact", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("contact", { logged_in: false });
+    }
+  } catch {
+    res.render("contact", { logged_in: false });
+  }
+});
+
+app.get("/codeofconduct", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("codeofconduct", {
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } else {
+      res.render("codeofconduct", { logged_in: false });
+    }
+  } catch {
+    res.render("codeofconduct", { logged_in: false });
+  }
+});
+
+app.get("/routine", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("routine", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("routine", { logged_in: false });
+    }
+  } catch {
+    res.render("routine", { logged_in: false });
+  }
+});
+
+app.get("/photos", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("photos", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("photos", { logged_in: false });
+    }
+  } catch {
+    res.render("photos", { logged_in: false });
+  }
+});
+
+app.get("/videos", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("videos", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("videos", { logged_in: false });
+    }
+  } catch {
+    res.render("videos", { logged_in: false });
+  }
+});
+
+app.get("/calender", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("calender", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("calender", { logged_in: false });
+    }
+  } catch {
+    res.render("calender", { logged_in: false });
+  }
+});
+
+app.get("/cocur", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("cocur", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("cocur", { logged_in: false });
+    }
+  } catch {
+    res.render("cocur", { logged_in: false });
+  }
+});
+
+app.get("/dress", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("dress", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("dress", { logged_in: false });
+    }
+  } catch {
+    res.render("dress", { logged_in: false });
+  }
+});
+
+app.get("/apply", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("admission/apply", {
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } else {
+      res.render("admission/apply", { logged_in: false });
+    }
+  } catch {
+    res.render("admission/apply", { logged_in: false });
+  }
+});
+
+app.get("/applicant_dashboard", function (req, res) {
+  try {
+    if (
+      !req.session.user.isAuthenticated ||
+      req.session.user.account_type != "Applicant"
+    ) {
+      res.redirect("log_in");
+      return;
+    }
+  } catch {
+    res.redirect("log_in");
+    return;
+  }
+
   console.log(req.session.user);
-  res.render("admission/applicant_dashboard");
+  res.render("admission/applicant_dashboard", {
+    logged_in: req.session.user.isAuthenticated,
+  });
 });
 
 app.get("/log_in", async function (req, res) {
   res.render("log_in");
 });
 
+let uniqueNumbersArray = [0, 0];
+
 app.get("/exam", async function (req, res) {
   function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  
+
   let randomNumbers = new Set();
-  
+
   while (randomNumbers.size < 10) {
     let randomNumber = getRandomNumber(1, 19);
-  
-    // Check if the number is not in the set before adding
+
     if (!randomNumbers.has(randomNumber)) {
       randomNumbers.add(randomNumber);
     }
   }
-  
-  // Convert the Set to an array if needed
-  let uniqueNumbersArray = Array.from(randomNumbers);
 
-  
-    const query = `
-      SELECT * FROM "ACADEMIA_PLUS_NEW"."ADMISSION_QUESTIONS"
-      WHERE ID IN (${uniqueNumbersArray.join(', ')})`;
-    
-    const result = await run(query);
+  uniqueNumbersArray = Array.from(randomNumbers);
 
-   
-      const questions = result.data;
-
-      console.log(randomNumbers);
-      
-      res.render("admission/exam", { questions:questions });
-    
+  console.log(uniqueNumbersArray);
+  res.render("admission/exam");
 });
 
+app.get("/exam_started", async function (req, res) {
+  try {
+    const query = `
+      SELECT * FROM "ACADEMIA_PLUS_NEW"."ADMISSION_QUESTIONS"
+      WHERE ID IN (${uniqueNumbersArray.join(", ")})`;
+
+    const result = await run(query);
+
+    const questions = result.data;
+
+    console.log("Questions:", questions);
+
+    res.render("admission/exam_started", { questions: questions });
+  } catch (error) {
+    console.error("Error in /exam_started:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/result", async function (req, res) {
+  const result = await run(
+    'SELECT RESULT FROM "ACADEMIA_PLUS_NEW"."ADMISSION_RESULT" WHERE APPLICANT_ID= :applicant_id ORDER BY TIME ASC',
+    { applicant_id: req.session.user.id }
+  );
+  console.log(result.data);
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("admission/result", {
+        result: result.data,
+        logged_in: req.session.user.isAuthenticated,
+      });
+    } else {
+      res.redirect("log_in");
+    }
+  } catch {
+    res.redirect("log_in");
+  }
+});
 
 /////////////////////////////////////////////All Get Request///////////////////////////////////////////////////////////////
 
@@ -332,6 +578,57 @@ app.post("/login", async function (req, res) {
   } else {
     res.json({ reply: info.success, data: info.data });
   }
+});
+
+app.post("/exam_submit", async function (req, res) {
+  const answers = req.body;
+  const keys = Object.keys(answers).map(Number);
+
+  const values = Object.values(answers);
+
+  const query = `
+      SELECT ANSWER FROM "ACADEMIA_PLUS_NEW"."ADMISSION_QUESTIONS"
+      WHERE ID IN (${keys.join(", ")})`;
+
+  const result = await run(query);
+  console.log("result.data:", result.data);
+  const correct_answers = result.data.map((item) => item[0]);
+  console.log("Correct:", correct_answers);
+  console.log("Keys:", keys);
+  console.log("Values:", values);
+  const matchingCount = values.reduce((count, value, index) => {
+    const correctAnswer = correct_answers[index];
+
+    if (value === correctAnswer) {
+      return count + 1;
+    } else {
+      return count;
+    }
+  }, 0);
+
+  console.log("Matching Count:", matchingCount);
+
+  await run(
+    `
+      INSERT INTO ADMISSION_RESULT (
+        APPLICANT_ID, RESULT, TIME
+      ) VALUES (
+        :applicant_id, :result, :time
+      )`,
+    {
+      applicant_id: req.session.user.id,
+      result: matchingCount,
+      time: Date.now().toString(),
+    }
+  );
+
+  res.redirect("/result");
+});
+
+app.post("/logout", async function (req, res) {
+  req.session.user.isAuthenticated = false;
+
+  res.redirect("/index");
 });
 
 app.listen(3000);
