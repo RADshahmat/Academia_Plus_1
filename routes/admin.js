fd7e61f0f4adb37e60744e942f305de16a761dbc
baten005 <134430653+baten005@users.noreply.github.com>
2024-02-06 21:13:53 +0600

    kaka

--- routes/admin.js
+++ routes/admin.js
@@ -1,72 +1,88 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require('fs');
 const { run } = require("../db/db");
 const upload = require("../multer/multer");
 
-router.get("/online_admission_handler",async function(req,res){
+router.get("/online_admission_handler", async function (req, res) {
+  const exam_stat = await run("SELECT * FROM EXAM");
+  console.log(exam_stat);
 
-     const exam_stat=await run("SELECT * FROM EXAM");
-     console.log(exam_stat);
-
-
-    res.render("admin_control/online_admission_handler",{exam_stat:exam_stat.data});
-
+  res.render("admin_control/online_admission_handler", {
+    exam_stat: exam_stat.data,
+  });
 });
 
+router.post("/online_exam_start", async function (req, res) {
+  const det = req.body;
 
-
-router.post("/online_exam_start", async function(req,res){
-  const det=req.body;
-  
   console.log(det.online_exam_start);
 
-    const result = await run(`UPDATE EXAM SET EXAM_STAT = ${det.online_exam_start}`);
+  const result = await run(
+    `UPDATE EXAM SET EXAM_STAT = ${det.online_exam_start}`
+  );
+});
 
-})
-
-router.get("/library_management",async function(req,res){
-
-
-res.render("admin_control/library_management");
-
+router.get("/library_management", async function (req, res) {
+  res.render("admin_control/library_management");
 });
 router.get("/sms", async function (req, res) {
   res.render("admin_control/sms");
 });
-router.get("/book_management",async function(req,res){
-const data= await run(`select * from BOOKS`);
-console.log(data);
+router.get("/book_management", async function (req, res) {
+  const data = await run(`select * from BOOKS`);
+  console.log(data);
 
-  res.render("admin_control/book_management",{books_info:data.data});
-  
-  });
+  res.render("admin_control/book_management", { books_info: data.data });
+});
 
-  router.get("/library_card",async function(req,res){
+router.get("/library_card", async function (req, res) {
+  res.render("admin_control/library_card");
+});
 
-
-    res.render("admin_control/library_card");
-    
-    });
-  
-    router.post("/add_books",upload.single("fileInput"),async function(req,res){
-      const data = req.body;
-      const file_name = req.file.filename;
-      console.log(data,file_name);
-      const feedback=await run(       `
+router.post(
+  "/add_books",
+  upload.single("fileInput"),
+  async function (req, res) {
+    const data = req.body;
+    const file_name = req.file.filename;
+    console.log(data, file_name);
+    const feedback = await run(
+      `
       INSERT INTO BOOKS (
         BOOK_NAME, AUTHOR, TYPE, BOOK_FILE
       ) VALUES (
         :book_name, :author, :type, :book_file
       )`,
-          {
-            book_name: data.bookTitle,
-            author: data.author,
-            type: data.type,
-            book_file: file_name,  
-          })
-      res.redirect("book_management");
-      
-      });  
-  
+      {
+        book_name: data.bookTitle,
+        author: data.author,
+        type: data.type,
+        book_file: file_name,
+      }
+    );
+    res.redirect("book_management");
+  }
+);
 
-module.exports= router;
+router.post("/delete_books", async (req, res) => {
+  const filename1 = req.body.book_name;
+  const folderPath = "uploadimage";
+
+  const filePath = path.join(__dirname, "..", folderPath, filename1);
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+    console.log(`File ${filename1} deleted successfully.`);
+    const det = await run(`DELETE FROM BOOKS WHERE BOOK_FILE= :book_name`, {
+      book_name: filename1,
+    });
+    console.log(det);
+  } else {
+    console.log(`File ${filename} not found.`);
+  }
+  res.redirect("/book_management");
+  console.log(filePath);
+});
+
+module.exports = router;
--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {run}=require ("../db/db")
+const path = require("path");
+
+const { run } = require("../db/db");
+const uploadFolder = "uploadimage";
 router.get("/studentsdashboard", function (req, res) {
-    /* try {
+  /* try {
        if (
          req.session.user.isAuthenticated ||
          req.session.user.account_type == "Applicant"
@@ -15,14 +18,13 @@ router.get("/studentsdashboard", function (req, res) {
        return;
      }
    */
-     console.log(req.session.user);
-     res.render("students/studentdashboard", {
-       
-       logged_in: req.session.user.isAuthenticated,
-     });
-   });
-   router.get("/student_management", function (req, res) {
-     /* try {
+  console.log(req.session.user);
+  res.render("students/studentdashboard", {
+    logged_in: req.session.user.isAuthenticated,
+  });
+});
+router.get("/student_management", function (req, res) {
+  /* try {
         if (
           req.session.user.isAuthenticated ||
           req.session.user.account_type == "Applicant"
@@ -35,24 +37,24 @@ router.get("/studentsdashboard", function (req, res) {
         return;
       }
     */
-      console.log(req.session.user);
-      res.render("admin_control/student_management", {
-        logged_in: req.session.user.isAuthenticated,
-      });
-    });
-    router.get("/loginadtc", function (req, res) {
-     try {
-       if (req.session.user.isAuthenticated) {
-         res.render("loginadtc", { logged_in: req.session.user.isAuthenticated });
-       } else {
-         res.render("loginadtc", { logged_in: false });
-       }
-     } catch {
-       res.render("loginadtc", { logged_in: false });
-     }
-   });
-   router.get("/classroom", function (req, res) {
-     /* try {
+  console.log(req.session.user);
+  res.render("admin_control/student_management", {
+    logged_in: req.session.user.isAuthenticated,
+  });
+});
+router.get("/loginadtc", function (req, res) {
+  try {
+    if (req.session.user.isAuthenticated) {
+      res.render("loginadtc", { logged_in: req.session.user.isAuthenticated });
+    } else {
+      res.render("loginadtc", { logged_in: false });
+    }
+  } catch {
+    res.render("loginadtc", { logged_in: false });
+  }
+});
+router.get("/classroom", function (req, res) {
+  /* try {
         if (
           req.session.user.isAuthenticated ||
           req.session.user.account_type == "Applicant"
@@ -65,14 +67,14 @@ router.get("/studentsdashboard", function (req, res) {
         return;
       }
     */
-      console.log(req.session.user);
-      res.render("students/classroom", {
-        logged_in: req.session.user.isAuthenticated,
-      });
-    });
-   
-    router.get("/chatbot", function (req, res) {
-     /* try {
+  console.log(req.session.user);
+  res.render("students/classroom", {
+    logged_in: req.session.user.isAuthenticated,
+  });
+});
+
+router.get("/chatbot", function (req, res) {
+  /* try {
         if (
           req.session.user.isAuthenticated ||
           req.session.user.account_type == "Applicant"
@@ -85,13 +87,13 @@ router.get("/studentsdashboard", function (req, res) {
         return;
       }
     */
-      console.log(req.session.user);
-      res.render("students/chatbot", {
-        logged_in: req.session.user.isAuthenticated,
-      });
-    });
-    router.get("/libraryStudent",async function (req, res) {
-     /* try {
+  console.log(req.session.user);
+  res.render("students/chatbot", {
+    logged_in: req.session.user.isAuthenticated,
+  });
+});
+router.get("/libraryStudent", async function (req, res) {
+  /* try {
         if (
           req.session.user.isAuthenticated ||
           req.session.user.account_type == "Applicant"
@@ -104,11 +106,28 @@ router.get("/studentsdashboard", function (req, res) {
         return;
       }
     */
-      const data= await run(`select * from BOOKS`);
-      console.log(data);
-      console.log(req.session.user);
-      res.render("students/libraryStudent", {
-        logged_in: req.session.user.isAuthenticated,books_info:data.data
-      });
-    });
-    module.exports = router;
+  const data = await run(`select * from BOOKS`);
+  console.log(data);
+  console.log(req.session.user);
+  res.render("students/libraryStudent", {
+    logged_in: req.session.user.isAuthenticated,
+    books_info: data.data,
+  });
+});
+
+router.post("/download_books", (req, res) => {
+  const filename = req.body.book_name;
+  const filePath = path.join(__dirname, "..", uploadFolder, filename);
+  console.log(filePath);
+  const fileExists = require("fs").existsSync(filePath);
+
+  if (!fileExists) {
+    return res.status(404).send("File not found");
+  }
+
+  res.setHeader("Content-disposition", "attachment; filename=" + filename);
+  res.setHeader("Content-type", "application/pdf");
+  const fileStream = require("fs").createReadStream(filePath);
+  fileStream.pipe(res);
+});
+module.exports = router;
