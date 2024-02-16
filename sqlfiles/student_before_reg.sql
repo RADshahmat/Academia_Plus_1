7e8def31a5480ed342e0ce57c6e1f9d224918426
baten005 <134430653+baten005@users.noreply.github.com>
2024-02-17 01:49:59 +0600

    student table added , deleted node modules simply type npm install in the terminal it will install all the dependencys

---
+++ sqlfiles/student_before_reg.sql
@@ -0,0 +1,24 @@
+--------------------------------------------------------
+--  File created - Saturday-February-17-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table STUDENTS_BEFORE_REGISTRATION
+--------------------------------------------------------
+
+  CREATE TABLE "ACADEMIA_PLUS_NEW"."STUDENTS_BEFORE_REGISTRATION" 
+   (	"ID" VARCHAR2(6 BYTE), 
+	"C1" VARCHAR2(200 BYTE), 
+	"C2" VARCHAR2(200 BYTE), 
+	"C3" VARCHAR2(200 BYTE), 
+	"C4" VARCHAR2(200 BYTE), 
+	"C5" VARCHAR2(200 BYTE), 
+	"C6" VARCHAR2(200 BYTE), 
+	"C7" VARCHAR2(200 BYTE), 
+	"C8" VARCHAR2(200 BYTE), 
+	"C9" VARCHAR2(200 BYTE), 
+	"C10" VARCHAR2(200 BYTE), 
+	"C11" VARCHAR2(200 BYTE), 
+	"C12" VARCHAR2(200 BYTE), 
+	"C13" VARCHAR2(200 BYTE), 
+	"C14" VARCHAR2(20 BYTE)
+   ) 
---
+++ sqlfiles/students.sql
@@ -0,0 +1,17 @@
+CREATE TABLE students (
+    id VARCHAR2(6) PRIMARY KEY,
+    student_name1 VARCHAR2(255),
+    student_name2 VARCHAR2(255),
+    phone_no VARCHAR2(255),
+    father_name VARCHAR2(255),
+    mother_name VARCHAR2(255),
+    present_address VARCHAR2(255),
+    permanent_address VARCHAR2(255),
+    dob VARCHAR2(255),
+    class VARCHAR2(255),
+    image VARCHAR2(255),
+    email VARCHAR2(255),
+    city VARCHAR2(255),
+    state VARCHAR2(255),
+    zipcode VARCHAR2(255)
+);
